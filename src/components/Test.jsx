a12417d25cacf967e9ae0022b0fa4b116a66a603
Kanitelk <derodroll@gmail.com>
2020-08-04 17:35:21 +0300

    add some Typescript

--- src/components/Test.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Test = ({test, children}) => {
-return <div className={test}>{children}</div>;
-};
-
-export default Test;
--- src/components/TodoList/TodoItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TodoItem = ({id, title, completed, toggle, toggler}) => {
-  return <div onClick={toggler} className={`todo-item ${completed ? 'completed' : ''}`}>
-    <p>{title}</p>
-  </div>
-}
-
-export default TodoItem;
